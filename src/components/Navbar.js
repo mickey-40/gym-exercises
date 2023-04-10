d1ad6a10b5bb5bc69f4443dc04c720824e6ac1e1
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-10 17:16:57 -0500

    setting up Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+
+import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+      <Link to='/' style={{ textDecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625' }}>
+        <img src={Logo} alt='Site Logo' style={{ width: '48px', height: '48px', margin: '0 20px'}}  />
+      </Link>
+      <Stack>
+        <Link to='/'>Home</Link>
+        <a href='#exercises' style={{ textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
+      </Stack>
+    </Stack>
   )
 }
 
