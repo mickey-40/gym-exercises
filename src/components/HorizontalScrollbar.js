0bbc9634e3e81d19620f27dc8a0d6c41b69edb71
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-12 07:49:50 -0500

    working on horizontalscrollbar.js

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+
+import BodyPart from './BodyPart'
 
 
-const HorizontalScrollbar = ( { data }) => {
+const HorizontalScrollbar = ( { data, bodyPart, setBodyPart }) => {
+
+
   console.log('data', data)
   return (
-    <Box>
+    <ScrollMenu>
       {data.map((item) => (
         <Box 
           key={item.id || item}
@@ -13,10 +18,10 @@ const HorizontalScrollbar = ( { data }) => {
           title={item.id || item}
           m='0 40px'
         >
-        {item}
+         <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
       ))}
-    </Box>
+    </ScrollMenu>
   )
 }
 
