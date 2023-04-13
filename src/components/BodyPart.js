6045c2fa29491b61f5af7fe0bdbd51b141b39cce
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-13 12:31:21 -0500

    finished scroll menu

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stack } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 
 import Icon from '../assets/icons/gym.png'
 
@@ -19,8 +19,13 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
           cursor: 'pointer',
           gap: '47px'
       }}
+      onClick={()=>{
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
+      }}
     >
       <img src={Icon} alt='dumbell' style={{ width: '40px', height: '40px'}} />
+      <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
     </Stack>
   )
 }
