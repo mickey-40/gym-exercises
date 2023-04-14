156e42e26c3b7b3d0bb353bb568e46cb2d86e223
mickey-40 <mickeyarnold53@yahoo.com>
2023-04-14 10:52:17 -0500

    Added Detail.js, Exercise.js, and SimilarExercises.js

---
+++ src/components/Detail.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Typography, Stack, Button } from '@mui/material'
+
+import BodyPartImage from '../assets/icons/body-part.png'
+import TargetImage from '../assets/icons/target.png'
+import Equipment from '../assets/icons/equipment.png'
+
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+
+  return (
+    <Stack gap='60px' sx={{ flexDirection: { lg: 'row'}, p: '20px', alignItems: 'center'}}>
+      <img scr={gifUrl} alt={name} loading='lazy' className='detail-image' />
+      <Stack sx={{ gap: {lg: '35px', xs: '20px'}}}>
+        <Typography>
+          {name}
+        </Typography>
+        <Typography>
+          Exercises keep you strong. {name} {` `}
+
+          is one of the best
+          exercises to target your {target }. It will help you improve your 
+          mood and gain energy.
+        </Typography>
+      </Stack>
+    </Stack>
+  )
+}
+
+export default Detail
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+
+import { exerciseOptions, fetchData } from '../utils/fetchData'
+
 
 const ExerciseDetail = () => {
+
+  const [exerciseDetail, setExerciseDetail ] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      // const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+      console.log({exerciseDetailData})
+      setExerciseDetail(exerciseDetailData);
+
+    }
+    fetchExercisesData()
+  }, [id])
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
